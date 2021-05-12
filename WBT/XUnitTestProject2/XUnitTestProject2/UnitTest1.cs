ced29271aa908f92d16e18e6b10845615efd4b63
Dmytrenko-Roman <savitarreus@gmail.com>
2021-05-12 17:42:15 +0300

    ❄️ Tests on MBF added ❄️

--- WBT/XUnitTestProject2/XUnitTestProject2/UnitTest1.cs
+++ WBT/XUnitTestProject2/XUnitTestProject2/UnitTest1.cs
@@ -1,14 +1,86 @@
 using System;
 using Xunit;
+using IIG.BinaryFlag;
 
 namespace XUnitTestProject2
 {
-    public class UnitTest1
+    public class BinaryFlagTests
     {
         [Fact]
-        public void Test1()
+        // MBF - MultipleBinaryFlag
+        public void MBF_0_1_2_9_True()
         {
-
+            ulong value = 1;
+            Assert.Throws<ArgumentOutOfRangeException>(() => new MultipleBinaryFlag(value, true));
+        }
+        [Fact]
+        public void MBF_0_1_2_9_False()
+        {
+            ulong value = 1;
+            Assert.Throws<ArgumentOutOfRangeException>(() => new MultipleBinaryFlag(value, false));
+        }
+        [Fact]
+        public void MBF_0_1_3_2_9_True()
+        {
+            ulong value = 17179868705;
+            Assert.Throws<ArgumentOutOfRangeException>(() => new MultipleBinaryFlag(value, true));
+        }
+        [Fact]
+        public void MBF_0_1_3_2_9_False()
+        {
+            ulong value = 17179868705;
+            Assert.Throws<ArgumentOutOfRangeException>(() => new MultipleBinaryFlag(value, false));
+        }
+        [Fact]
+        public void MBF_0_1_3_4_5_9_True()
+        {
+            ulong value = 32;
+            MultipleBinaryFlag obj = new MultipleBinaryFlag(value, true);
+            Assert.NotNull(obj);
+        }
+        [Fact]
+        public void MBF_0_1_3_4_5_9_False()
+        {
+            ulong value = 32;
+            MultipleBinaryFlag obj = new MultipleBinaryFlag(value, false);
+            Assert.NotNull(obj);
+        }
+        [Fact]
+        public void MBF_0_1_3_4_6_8_9_True()
+        {
+            ulong value = 66;
+            MultipleBinaryFlag obj = new MultipleBinaryFlag(value, true);
+            Assert.NotNull(obj);
+        }
+        [Fact]
+        public void MBF_0_1_3_4_6_8_9_False()
+        {
+            ulong value = 66;
+            MultipleBinaryFlag obj = new MultipleBinaryFlag(value, false);
+            Assert.NotNull(obj);
+        }
+        [Fact]
+        public void MBF_0_1_3_4_6_7_9_True()
+        {
+            ulong value = 64;
+            MultipleBinaryFlag obj = new MultipleBinaryFlag(value, true);
+            Assert.NotNull(obj);
+        }
+        [Fact]
+        public void MBF_0_1_3_4_6_7_9_False()
+        {
+            ulong value = 64;
+            MultipleBinaryFlag obj = new MultipleBinaryFlag(value, false);
+            Assert.NotNull(obj);
+        }
+        [Fact]
+        public void SetFlag_0_1_2_3_12_True()
+        {
+            ulong value = 32;
+            MultipleBinaryFlag obj = new MultipleBinaryFlag(value, true); ;
+            obj.SetFlag(20);
+            bool? result = obj.GetFlag();
+            Assert.Equal(result, true);
         }
     }
 }
